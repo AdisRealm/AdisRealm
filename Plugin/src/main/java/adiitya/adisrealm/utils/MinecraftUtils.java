d05ff9008ceef9312d384bf7b14d36f6e2176bfb
AdityaSF <A44itya@hotmail.com>
2018-10-17 08:55:02 -0600

    Remove unused dependencies

--- Plugin/src/main/java/adiitya/adisrealm/utils/MinecraftUtils.java
+++ Plugin/src/main/java/adiitya/adisrealm/utils/MinecraftUtils.java
@@ -3,17 +3,14 @@ package adiitya.adisrealm.utils;
 import adiitya.mojang4j.MojangAPI;
 import adiitya.mojang4j.UserProfile;
 import lombok.experimental.UtilityClass;
-import org.bukkit.Bukkit;
 
 import java.util.Optional;
 import java.util.UUID;
-import java.util.logging.Logger;
 
 @UtilityClass
 public class MinecraftUtils {
 
 	private static MojangAPI mojang = MojangAPI.getInstance();
-	private final Logger log = Bukkit.getLogger();
 
 	public boolean playerExists(UUID uuid) {
 		return mojang.getProfile(uuid).isPresent();
