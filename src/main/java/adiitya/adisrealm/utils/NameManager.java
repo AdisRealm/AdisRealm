7beaa6e774008f4d7d0cedd936d0a03d5511e0c3
AdityaSF <A44itya@hotmail.com>
2018-12-06 17:04:22 -0600

    Add javadoc for NameManager

--- src/main/java/adiitya/adisrealm/utils/NameManager.java
+++ src/main/java/adiitya/adisrealm/utils/NameManager.java
@@ -12,6 +12,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * This class handles all name related function.
+ */
 @UtilityClass
 public class NameManager {
 
@@ -28,6 +31,12 @@ public class NameManager {
 				.forEach(p -> p.setPlayerListName(getTabName(p.getUniqueId())));
 	}
 
+	/**
+	 * Sets the tab prefix of the user. Tab names are updated every tick.
+	 *
+	 * @param uuid The player
+	 * @param prefix The prefix
+	 */
 	public void setTabPrefix(UUID uuid, String prefix) {
 
 		if (prefix.isEmpty())
@@ -36,6 +45,13 @@ public class NameManager {
 			tabPrefixes.put(uuid, prefix);
 	}
 
+	/**
+	 * Gets the users tab name. This includes the tab prefix and colored name.
+	 *
+	 * @param uuid the player
+	 *
+	 * @return The formatted tab name
+	 */
 	public String getTabName(UUID uuid) {
 
 		OfflinePlayer p = Bukkit.getOfflinePlayer(uuid);
@@ -46,35 +62,63 @@ public class NameManager {
 		return tabPrefixes.getOrDefault(uuid, "") + getColoredName(p.getName());
 	}
 
-	public static void setColor(String name, ChatColor color) {
+	/**
+	 * Sets the name color of the player
+	 *
+	 * @param name The name of the player
+	 * @param color The color
+	 */
+	public void setColor(String name, ChatColor color) {
 
 		Team team = getTeam(color);
 		team.addEntry(name);
 	}
 
-	public static ChatColor getColor(String name) {
+	/**
+	 * Gets the color of the player
+	 *
+	 * @param name The name of the player
+	 *
+	 * @return The color else white if the user has no color
+	 */
+	public ChatColor getColor(String name) {
 
 		Team team = scoreboard.getEntryTeam(name);
 
 		return team == null ? ChatColor.WHITE : team.getColor();
 	}
 
-	public static String getColoredName(String name) {
+	/**
+	 * Gets the colored name of a player
+	 *
+	 * @param name The name of the player
+	 *
+	 * @return The colored name of the player
+	 */
+	public String getColoredName(String name) {
 		return getColor(name).toString() + name + "§r";
 	}
 
-	public static boolean isColor(String name, ChatColor color) {
+	/**
+	 * Checks whether the player has a specific name color
+	 *
+	 * @param name The name of the player
+	 * @param color The color to check
+	 *
+	 * @return Whether the player has the color
+	 */
+	public boolean isColor(String name, ChatColor color) {
 		return getColor(name) == color;
 	}
 
-	private static Team getTeam(ChatColor color) {
+	private Team getTeam(ChatColor color) {
 
 		Team team = scoreboard.getTeam(color.name());
 
 		return team == null ? createTeam(color) : team;
 	}
 
-	private static Team createTeam(ChatColor color) {
+	private Team createTeam(ChatColor color) {
 
 		Team team = scoreboard.getTeam(color.name());
 
